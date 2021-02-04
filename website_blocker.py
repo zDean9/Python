0deaba874e9534fabda888d38ab066cf76d72a75
zDean9 <72355610+zDean9@users.noreply.github.com>
2021-02-04 18:49:39 +0000

    Add files via upload
    Website blocker made in python. When trying to excute program. This was the error that showed up. 
    
    Traceback (most recent call last):
      File "C:\Users\ziadi\OneDrive\Desktop\Python Project\website_blocker.py", line 14, in <module>
        with open(hosts_path, "r+") as file:
    FileNotFoundError: [Errno 2] No such file or directory: '/etc/hosts'

---
+++ website_blocker.py
@@ -0,0 +1,32 @@
+#Website Blocker# 
+
+
+import time 
+from datetime import datetime as dt 
+hosts_path = r"/etc/hosts"  # r is for raw string 
+hosts_temp = "hosts"
+redirect = "127.0.0.1"
+web_sites_lists = ["www.facebook.com", "facebook.com"] #users can modify lists of websites they want to block
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,22):
+        print("Working hours")
+        with open(hosts_path, "r+") as file:
+            content = file.read()
+            for website in content:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
+        
+    else:
+        print("Fun time")
+        with open(hosts_path, "r+") as file:
+            content = file.read.lines()
+            file.seek(0) #reset the pointer tp the top of the text file
+            
+            for line in content:
+                if not any('www.facebook.com'):
+                    file.write(line)
+                    file.truncate
+                    time.sleep(5)
